a3591166023dd2bb2c46421626f157fbaeb50495
Bethany Chang <bethansc@uci.edu>
2023-05-22 17:32:14 -0700

    If Else Statements

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a Fact";
   }
 });
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "Impossible Year";
+}
